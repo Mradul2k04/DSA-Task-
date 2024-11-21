a3da9f1b026d4c54a2e9dd353a50898f97d6a8a0
Mradul Sharma <mradul.btech.cs22@gmail.com>
2024-11-21 21:55:39 +0530

    Time: 21 ms (78.95%), Space: 72.2 MB (30.12%) - LeetHub

--- 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.java
+++ 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.java
@@ -1,62 +1,65 @@
-import java.util.*;
-
 class Solution {
-    private void dfs(int r, int c, String dir, int[][] vis, Map<String, Integer> mp) {
-        int n = vis.length;
-        int m = vis[0].length;
-
-        if (r < 0 || c < 0 || r >= n || c >= m) return;
-        if (mp.containsKey(r + "," + c)) return;
-
-        vis[r][c] = 1;
-
-        switch (dir) {
-            case "r":
-                dfs(r, c + 1, "r", vis, mp);
-                break;
-            case "l":
-                dfs(r, c - 1, "l", vis, mp);
-                break;
-            case "u":
-                dfs(r - 1, c, "u", vis, mp);
-                break;
-            case "d":
-                dfs(r + 1, c, "d", vis, mp);
-                break;
-        }
-    }
-
     public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
-        int[][] vis = new int[m][n];
-        Map<String, Integer> mp = new HashMap<>();
+        int cnt=m*n;
+        int[][] arr=new int[m][n];
 
-        for (int[] guard : guards) {
-            int x = guard[0], y = guard[1];
-            mp.put(x + "," + y, 1);
-            vis[x][y] = 1;
+        //Guard value=2;
+        for(int i=0; i<guards.length; i++){
+            arr[guards[i][0]][guards[i][1]]=2; 
+            cnt--; 
+        }
+        //Wall value=3
+        for(int i=0; i<walls.length; i++){
+            arr[walls[i][0]][walls[i][1]]=3;
+            cnt--; 
         }
 
-        for (int[] wall : walls) {
-            int x = wall[0], y = wall[1];
-            mp.put(x + "," + y, 1);
-            vis[x][y] = 1;
+
+        //Traversing the matrix array
+        for(int i=0; i<guards.length; i++){
+            int r=guards[i][0], c=guards[i][1];
+            
+            //r: Row fixed
+            ////////////////////////////////////////////
+            //LEFT::
+            for(int j=c-1; j>=0; j--){ //n: Column wise move                               
+                if(arr[r][j]==2 || arr[r][j]==3){
+                    break;
+                }            
+                if(arr[r][j]==0)cnt--;     
+                arr[r][j]=1;                                
+            } 
+            //RIGHT::     
+            for(int j=c+1; j<n; j++){ //n: Column wise move
+                if(arr[r][j]==2 || arr[r][j]==3){
+                    break;
+                } 
+                if(arr[r][j]==0)cnt--;               
+                arr[r][j]=1;                                  
+            }      
+
+            //c: Column fixed
+            ////////////////////////////////////////////
+            //UP::
+            for(int j=r-1; j>=0; j--){ //m: Row wise move
+                if(arr[j][c]==2 || arr[j][c]==3){
+                    break;
+                }            
+                if(arr[j][c]==0)cnt--;    
+                arr[j][c]=1;                                 
+            }  
+            //DOWN::
+            for(int j=r+1; j<m; j++){ //m: Row wise move                
+                if(arr[j][c]==2 || arr[j][c]==3){
+                    break;
+                }                                                
+                if(arr[j][c]==0) cnt--;                  
+                arr[j][c]=1;                
+            }     
+
         }
 
-        for (int[] guard : guards) {
-            int r = guard[0], c = guard[1];
-            dfs(r, c + 1, "r", vis, mp);
-            dfs(r, c - 1, "l", vis, mp);
-            dfs(r + 1, c, "d", vis, mp);
-            dfs(r - 1, c, "u", vis, mp);
-        }
 
-        int count = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (vis[i][j] == 0) count++;
-            }
-        }
-
-        return count;
+        return cnt;
     }
 }
