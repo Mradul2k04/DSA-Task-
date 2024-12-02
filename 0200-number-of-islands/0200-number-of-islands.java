5b0f916b553a2f262eb250de1353d6ca84b21e4d
Mradul Sharma <mradul.btech.cs22@gmail.com>
2024-12-02 21:47:54 +0530

    Time: 3 ms (86.83%), Space: 49.2 MB (80.31%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,40 +1,23 @@
-class Solution {
-    public int numIslands(char[][] grid) {
-        int islands = 0;
-        int rows = grid.length;
-        int cols = grid[0].length;
-        Set<String> visited = new HashSet<>();
-
-        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                if (grid[r][c] == '1' && !visited.contains(r + "," + c)) {
-                    islands++;
-                    bfs(grid, r, c, visited, directions, rows, cols);
-                }
-            }
-        }
-
-        return islands;        
-    }
-
-    private void bfs(char[][] grid, int r, int c, Set<String> visited, int[][] directions, int rows, int cols) {
-        Queue<int[]> q = new LinkedList<>();
-        visited.add(r + "," + c);
-        q.add(new int[]{r, c});
-
-        while (!q.isEmpty()) {
-            int[] point = q.poll();
-            int row = point[0], col = point[1];
-
-            for (int[] direction : directions) {
-                int nr = row + direction[0], nc = col + direction[1];
-                if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] == '1' && !visited.contains(nr + "," + nc)) {
-                    q.add(new int[]{nr, nc});
-                    visited.add(nr + "," + nc);
-                }
-            }
-        }
-    }    
+class Solution{
+public void dfs(char[][]grid,int i,int j){
+int m=grid.length,n=grid[0].length;
+if(i<0||j<0||i>=m||j>=n||grid[i][j]=='0')return;
+grid[i][j]='0';
+dfs(grid,i+1,j);
+dfs(grid,i-1,j);
+dfs(grid,i,j+1);
+dfs(grid,i,j-1);
+}
+public int numIslands(char[][]grid){
+int m=grid.length,n=grid[0].length,count=0;
+for(int i=0;i<m;i++){
+for(int j=0;j<n;j++){
+if(grid[i][j]=='1'){
+count++;
+dfs(grid,i,j);
+}
+}
+}
+return count;
+}
 }
