5d5e36dbdf3a3fc781cccc9838c7360cc1e0f74c
Mradul Sharma <mradul.btech.cs22@gmail.com>
2025-01-12 20:41:16 +0530

    Time: 15 ms (83.4%), Space: 45.9 MB (37.76%) - LeetHub

--- 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.java
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.java
@@ -1,38 +1,28 @@
-import java.util.Stack;
-
 class Solution {
-    public boolean canBeValid(String s, String locked) {
-        int stringLength = s.length();
-        if (stringLength % 2 == 1) {
+    public boolean canBeValid(String inputString, String lockStatus) {
+        if (inputString.length() % 2 != 0) 
             return false;
-        }
-        Stack<Integer> openIndices = new Stack<>();
-        Stack<Integer> unlockedIndices = new Stack<>();
 
-        for (int i = 0; i < stringLength; i++) {
-            if (locked.charAt(i) == '0') {
-                unlockedIndices.push(i);
-            } else if (s.charAt(i) == '(') {
-                openIndices.push(i);
-            } else if (s.charAt(i) == ')') {
-                if (!openIndices.isEmpty()) {
-                    openIndices.pop();
-                } else if (!unlockedIndices.isEmpty()) {
-                    unlockedIndices.pop();
-                } else {
-                    return false;
-                }
-            }
-        }
-        while (!openIndices.isEmpty() && !unlockedIndices.isEmpty() &&
-               openIndices.peek() < unlockedIndices.peek()) {
-            openIndices.pop();
-            unlockedIndices.pop();
-        }
-        if (openIndices.isEmpty() && !unlockedIndices.isEmpty()) {
-            return unlockedIndices.size() % 2 == 0;
+        int balanceCount = 0;
+        for (int i = 0; i < inputString.length(); i++) {
+            if (lockStatus.charAt(i) == '0' || inputString.charAt(i) == '(') 
+                balanceCount++;
+            else 
+                balanceCount--;
+            if (balanceCount < 0) 
+                return false;
         }
 
-        return openIndices.isEmpty();
+        balanceCount = 0;
+        for (int i = inputString.length() - 1; i >= 0; i--) {
+            if (lockStatus.charAt(i) == '0' || inputString.charAt(i) == ')') 
+                balanceCount++;
+            else 
+                balanceCount--;
+            if (balanceCount < 0) 
+                return false;
+        }
+
+        return true;
     }
 }
